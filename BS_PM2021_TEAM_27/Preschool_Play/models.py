db59b1d499f8ddbbe3fd4529b48ac461af56d852
YehonatamBinyamini <yonile2106@gmail.com>
2021-03-21 20:36:58 +0200

    BSPM2021T27-61 created a model to save messages in the database.

--- BS_PM2021_TEAM_27/Preschool_Play/models.py
+++ BS_PM2021_TEAM_27/Preschool_Play/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent', on_delete=models.SET_NULL, null=True)
+    receiver = models.ForeignKey(User, related_name='received', on_delete=models.SET_NULL, null=True)
+    subject = models.CharField(max_length=50, blank=True)
+    body = models.CharField(max_length=250, blank=True)
+    sent_date = models.DateTimeField(auto_now_add=True)
+    deleted_by_sender = models.BooleanField(default=False)
+    deleted_by_receiver = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.sender + ' to ' + self.receiver)
