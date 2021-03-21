910060446f64faae2877ca3067a1434a4241a475
RotemLibrati <rotemlibrati1@gmail.com>
2021-03-21 20:12:26 +0200

    BSPM2021T27-2 created user profile model to hold extra information per user.

--- BS_PM2021_TEAM_27/Preschool_Play/models.py
+++ BS_PM2021_TEAM_27/Preschool_Play/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import timedelta, datetime
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    TYPES = (('parent', 'parent'), ('teacher', 'teacher'))
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE, blank=True, null=True)
+    son = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name='dad')
+    address = models.CharField(max_length=100, default='')
+    age = models.IntegerField(default=0)
+    points = models.IntegerField(default=0)
+    type = models.CharField(max_length=10, choices=TYPES, default='parent')
+    is_admin = models.BooleanField(default=False)
+    suspension_time = models.DateTimeField(default=datetime(2000, 1, 1))
+    total_minutes = models.FloatField(default=0)
+    last_login = models.DateTimeField(default=datetime(2000, 1, 1))
+    rank = models.IntegerField(default=0)
+    level = models.IntegerField(default=1)
+    limit = models.DateTimeField(default=datetime(2000, 1, 1))
+
+
+
