4cbdea85e803afb19468610f7aabaf3a8a2e85e9
Eilonc285 <50795839+Eilonc285@users.noreply.github.com>
2021-05-08 20:02:40 +0300

    attempt bug fix: firefox driver

--- BS_PM2021_TEAM_27/Preschool_Play/tests.py
+++ BS_PM2021_TEAM_27/Preschool_Play/tests.py
@@ -435,8 +435,8 @@ class TestIntegrationWithSelenium(StaticLiveServerTestCase):
         # geckodriver_autoinstaller.install(True)
         # path = os.getcwd() + '/geckodriver-linux64'
         # firefox_binary = FirefoxBinary(path)
-        path = os.path.dirname(os.path.realpath('./geckodriver-linux64'))
-        self.browser = webdriver.Firefox(executable_path=path)
+        # path = os.path.dirname(os.path.realpath('./geckodriver-linux64'))
+        self.browser = webdriver.Firefox(executable_path='./geckodriver-linux64')
 
         self.admin_user = User.objects.create_user('admin', 'admin@test.com')
         self.admin_user.set_password('qwerty246')
