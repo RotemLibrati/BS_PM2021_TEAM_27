7c6af5eeb14739b4593de9afc9c105d15a4bc607
Eilonc285 <50795839+Eilonc285@users.noreply.github.com>
2021-06-02 12:57:10 +0300

    fix attempt missing firefix binary (not installed)

--- BS_PM2021_TEAM_27/Preschool_Play/tests_integration.py
+++ BS_PM2021_TEAM_27/Preschool_Play/tests_integration.py
@@ -14,8 +14,8 @@ class TestIntegrationWithSelenium(StaticLiveServerTestCase):
         opts = FirefoxOptions()
         if os.name != 'nt':
             driver = './geckodriver-linux64'
-            # opts.add_argument("--headless")
-        self.browser = webdriver.Firefox(executable_path=driver, options=opts)
+            opts.add_argument("--headless")
+        self.browser = webdriver.Firefox()
 
         self.admin_user = User.objects.create_user('admin', 'admin@test.com')
         self.admin_user.set_password('qwerty246')
