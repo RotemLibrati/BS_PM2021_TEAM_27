2ba5abcda46a36dcef9dba1e6f064b09b9e0a2eb
RotemLibrati <rotemlibrati1@gmail.com>
2021-06-03 15:04:48 +0300

    default address was changed, In enter to website we get the main page instead page 404.

--- BS_PM2021_TEAM_27/BS_PM2021_TEAM_27/urls.py
+++ BS_PM2021_TEAM_27/BS_PM2021_TEAM_27/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('preschoolplay/', include('Preschool_Play.urls')),
+    path(r'', include('Preschool_Play.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
