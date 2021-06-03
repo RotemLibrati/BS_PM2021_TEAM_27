c24c7b740eef566abc92234d8ea9d6d9023a4c15
Eilonc285 <50795839+Eilonc285@users.noreply.github.com>
2021-06-03 22:58:03 +0300

    config fix

--- BS_PM2021_TEAM_27/Preschool_Play/forms.py
+++ BS_PM2021_TEAM_27/Preschool_Play/forms.py
@@ -102,7 +102,7 @@ class DeleteUserForm(forms.Form):
         super(DeleteUserForm, self).__init__(*args, **kwargs)
         self.fields['child'].queryset = set1
 
-    child = forms.ModelChoiceField(queryset={})
+    child = forms.ModelChoiceField(queryset=Child.objects.none())
     password = forms.CharField(widget=forms.PasswordInput)
 
 
